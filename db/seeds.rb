ff866adac68c1f33ac40155d8554fe592e15dfb4
jonathon <ahnjon77@gmail.com>
2021-10-29 12:12:52 -0700

    new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,58 +5,117 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Group.create!([
+  {
+    name: "pickfordians",
+    image: "https://media.istockphoto.com/photos/group-of-active-mature-friends-in-park-stacking-hands-after-workout-picture-id1319764588?b=1&k=20&m=1319764588&s=170667a&w=0&h=4UREEAMGkABBhFUSh5-EdOgtX4Pl6lJHChx5MYSZ8X0="
+  },
+  {
+    name: "dani group",
+    image: "https://media.istockphoto.com/photos/group-of-active-mature-friends-in-park-stacking-hands-after-workout-picture-id1319764588?b=1&k=20&m=1319764588&s=170667a&w=0&h=4UREEAMGkABBhFUSh5-EdOgtX4Pl6lJHChx5MYSZ8X0="
+  }
+])
 
-group = Group.new(name: "ray and the koreas", image: "https://images1.apartments.com/i2/7OUL8eNa4oJC8QDYcrpY9PfS-eSF4Wga0NC3kGQGdhA/112/4601-pickford-st-los-angeles-ca-primary-photo.jpg?p=1")
-group.save 
-listing = Listing.new(
-  address: "8961 Cadillac Avenue",
+User.create!([
+  {
+    name: "raymond",
+    email: "raymond@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+    group_id: 1,
+    image: "https://i.kym-cdn.com/photos/images/newsfeed/000/970/542/3cd.jpg",
+},
+{
+  name: "james",
+  email: "james@gmail.com",
+  password: "password",
+  password_confirmation: "password",
   group_id: 1,
-  status: "pending contact",
-  url: "https://www.zillow.com/homedetails/8961-Cadillac-Ave-Los-Angeles-CA-90034/20491254_zpid/"
-)
-listing.save
-listing = Listing.new(
-  address: "4650 Mascot Street",
+  image: "https://i.kym-cdn.com/photos/images/newsfeed/000/970/542/3cd.jpg",
+},
+{
+  name: "jon",
+  email: "jon@gmail.com",
+  password: "password",
+  password_confirmation: "password",
   group_id: 1,
-  status: "denied",
-  url: "https://www.zillow.com/homedetails/4650-Mascot-St-Los-Angeles-CA-90019/2115431785_zpid/"
-)
-listing.save
-listing = Listing.new(
-  address: "4603 Pickford Street",
+  image: "https://i.kym-cdn.com/photos/images/newsfeed/000/970/542/3cd.jpg",
+},
+{
+  name: "nathan",
+  email: "nathan@gmail.com",
+  password: "password",
+  password_confirmation: "password",
   group_id: 1,
-  status: "confirmed",
-  url: "https://www.apartments.com/4601-pickford-st-los-angeles-ca/bj308e8/"
-)
-listing.save
-listing = Listing.new(
-  address: "1233 S Westgate Avenue",
-  group_id: 1,
-  status: "pending walkthrough",
-  url: "https://www.zillow.com/b/westgate-wonder-los-angeles-ca-BJmtgw/"
-)
-listing.save
-comment = Comment.new(
-  text: "real nice",
-  listing_id: 1,
-  user_id: 3
-)
-comment.save
-comment = Comment.new(
-  text: "i like this",
-  listing_id: 1,
-  user_id: 4
-)
-comment.save
-comment = Comment.new(
-  text: "apparently not interested in 4 tenants??",
-  listing_id: 1,
-  user_id: 4
-)
-comment.save
-comment = Comment.new(
-  text: "big place with 5 rooms, kinda cheap",
-  listing_id: 2,
-  user_id: 4
-)
-comment.save
+  image: "https://i.kym-cdn.com/photos/images/newsfeed/000/970/542/3cd.jpg",
+},
+{
+  name: "dani",
+  email: "dani@gmail.com",
+  password: "password",
+  password_confirmation: "password",
+  group_id: 2,
+  image: "https://i.kym-cdn.com/photos/images/newsfeed/000/970/542/3cd.jpg",
+},
+])
+
+Listing.create!([
+  {
+    address: "8961 cadillac ave",
+    group_id: 1,
+    status: "pending contact",
+    url: "https://www.apartments.com/8961-cadillac-ave-los-angeles-ca/5r85qzq/"
+  },
+  {
+    address: "4650 mascot  st",
+    group_id: 1,
+    status: "denied",
+    url: "https://www.zillow.com/homedetails/4650-Mascot-St-Los-Angeles-CA-90019/2115431785_zpid/"
+  },
+  {
+    address: "4603 pickford st",
+    group_id: 1,
+    status: "confirmed",
+    url: "https://www.zillow.com/homedetails/4601-Pickford-St-Los-Angeles-CA-90019/20602010_zpid/"
+  },
+  {
+    address: "1233 s westgate ave",
+    group_id: 1,
+    status: "pending walkthrough",
+    url: "https://www.apartments.com/1233-s-westgate-ave-los-angeles-ca/y14z29r/"
+  },
+  {
+    address: "1234 test street",
+    group_id: 2,
+    status: "does not exist",
+    url: "whatever.com"
+  },
+])
+
+Comment.create!([
+  {
+    text: "real nice",
+    listing_id: 1,
+    user_id: 1
+  },
+  {
+    text: "i like this",
+    listing_id: 1,
+    user_id: 2
+  },
+  {
+    text: "apparently not interested in 4 tenants???",
+    listing_id: 1,
+    user_id: 2
+  },
+  {
+    text: "big place iwht 5 rooms, kinda cheap",
+    listing_id: 2,
+    user_id: 2
+  },
+  {
+    text: "this place doesn't even exist",
+    listing_id: 5,
+    user_id: 5
+  }
+])
